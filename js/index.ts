58709b7463bd4a61a7c18904bfb7b2db5501f563
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-10-10 13:49:11 +0300

    Move Stitch Login to index page

--- js/index.ts
+++ js/index.ts
@@ -1,4 +1,6 @@
 import { DB } from './db';
+import './stitch/stitchIndex';
+import { loginAnonymous, hasLoggedInUser } from './stitch/stitchIndex';
 
 const db = new DB();
 //let loginButton: Element = document.querySelector("#login-button") as HTMLElement;
@@ -7,34 +9,38 @@ let loginPassword: HTMLInputElement = document.querySelector("#login-password") 
 let loginForm: HTMLFormElement = document.querySelector("#login-form") as HTMLFormElement;
 
 async function init() {
-    if (loginForm && loginUsername && loginPassword) {
-        loginForm.addEventListener('submit', (e: Event) => {
-            e.preventDefault();
-            db.loginUser(loginUsername.value, loginPassword.value)
-                .then((response) => {
-                    console.log(window.location);
-                    let location = window.location.href;
-                    //location = location.replace("index.html", "notes.html");
-                    //location = location + "/notes.html";
-                    if (location.includes("index.html")) {
-                        location = location.replace("index.html", "notes.html");
-                    } else {
-                        location = location + "notes.html";
-                    }
-                    window.location.href = location;
-                })
-                .catch((error) => {
-                    let errMessage: Element = document.querySelector("#error-message") as HTMLElement;
-                    errMessage.textContent = "Incorrect username or password or user does not exist.";
-                    console.log("user does not exist");
+  loginAnonymous().then(() => {
+    console.log('Anonymous user logged in: ', hasLoggedInUser());
+  });
 
-                });
+  if (loginForm && loginUsername && loginPassword) {
+    loginForm.addEventListener('submit', (e: Event) => {
+      e.preventDefault();
+      db.loginUser(loginUsername.value, loginPassword.value)
+        .then((response) => {
+          console.log(window.location);
+          let location = window.location.href;
+          //location = location.replace("index.html", "notes.html");
+          //location = location + "/notes.html";
+          if (location.includes("index.html")) {
+            location = location.replace("index.html", "notes.html");
+          } else {
+            location = location + "notes.html";
+          }
+          window.location.href = location;
+        })
+        .catch((error) => {
+          let errMessage: Element = document.querySelector("#error-message") as HTMLElement;
+          errMessage.textContent = "Incorrect username or password or user does not exist.";
+          console.log("user does not exist");
 
         });
-    } else {
-        console.log("lol nice try lmao");
 
-    }
+    });
+  } else {
+    console.log("lol nice try lmao");
+
+  }
 }
 init();
 // export { loginForm, loginUsername, loginPassword };
