42bc5bcd47f771135726bb69cff3b39f3df740aa
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-10-10 12:04:01 +0300

    Add Anonymous User login on app load

--- js/app.ts
+++ js/app.ts
@@ -5,3 +5,8 @@ import './registerpage';
 import './index';
 import './notespage';
 import './stitch/stitchIndex';
+import { loginAnonymous, hasLoggedInUser, getCurrentUser } from './stitch/stitchIndex';
+
+loginAnonymous().then( () => {
+  console.log('Anonymous user logged in: ', hasLoggedInUser());
+});
